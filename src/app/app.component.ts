5baf4cd31d7bc5c6d710f2e1764b6f5c8fcdeb46
Sukucee <magabokc@gmail.com>
2024-10-03 01:59:14 +0800

    1.1 Student List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularDATASTRUC';
+onInput($event: Event) {
+throw new Error('Method not implemented.');
+}
+  cartList: string[] = ["Laptop", "Computer"];
+  item: string = '';
+
+  addItems() {
+    this.cartList.push(this.item);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { StudentListComponent } from './student-list/student-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+    title: 'Student List',
+		component: StudentListComponent,
+		path: 'student-list',
+	},
+
+
+];
